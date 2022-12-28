17e006b5a8f078b65ef883e323e603c08dc198f7
Jaehoon Shin <jshin@c7r4s3.42seoul.kr>
2022-12-28 23:18:07 +0900

    221228_1118pm

--- Module00/ex01/PhoneBook.cpp
+++ Module00/ex01/PhoneBook.cpp
@@ -1,2 +1,13 @@
 #pragma once
+#include "PhoneBook.hpp"
 
+void PhoneBook::add(Contact contact)
+{
+	if (this->contactCount == 8)
+	{
+		this->contactCount = 0;
+		this->isFull = true;
+	}
+	this->contacts[contactCount] = contact;
+	this->contactCount++;
+}
--- Module00/ex01/main.cpp
+++ Module00/ex01/main.cpp
@@ -1,2 +1,60 @@
 #include "PhoneBook.hpp"
 
+int main(void)
+{
+	int			choice;
+	PhoneBook	phoneBook = PhoneBook();
+	std::string	firstName;
+	std::string	lastName;
+	std::string	nickName;
+	std::string	phoneNumber;
+	std::string	darkestSecret;
+	Contact		contact;
+
+	while (1)
+	{ 
+		std::cout << "#############################" << std::endl;
+		std::cout << "#   Select:                 #" << std::endl;
+		std::cout << "#   [1] Add a contact       #" << std::endl;
+		std::cout << "#   [2] Search contacts     #" << std::endl;
+		std::cout << "#   [3] Exit                #" << std::endl;
+		std::cout << "#############################" << std::endl;
+		std::cout << "input: ";
+		std::cin >> choice;
+		if (std::cin.fail())
+		{
+			std::cerr << "Invalid input" << std::endl;
+			exit(1);
+		}
+		switch (choice)
+		{
+			case 1:
+				std::cin.clear();
+				std::cin.ignore();
+				std::cout << "Enter the first name: ";
+				getline(std::cin, firstName);
+				contact.setFirstName(firstName);
+				std::cout << "Enter the last name: ";
+				getline(std::cin, lastName);
+				contact.setLastName(lastName);
+				std::cout << "Enter the nickname: ";
+				getline(std::cin, nickName);
+				contact.setNickName(nickName);
+				std::cout << "Enter the phone number: ";
+				getline(std::cin, phoneNumber);
+				contact.setPhoneNumber(phoneNumber);
+				std::cout << "Enter the darkest secret: ";
+				getline(std::cin, darkestSecret);
+				contact.setDarkestSecret(darkestSecret);
+				phoneBook.add(contact);
+				break ;
+			case 2:
+				phoneBook.search();
+				break;
+			case 3:
+				phoneBook.exit();
+				break ;
+		}
+	}
+	return 0;
+}
